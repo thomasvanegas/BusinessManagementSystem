7461d5fc475158ba73b517fc6ed91bc2d6be334f
thomasvanegas <thomascamilovanegasacevedo2018@gmail.com>
2022-01-15 19:47:49 -0500

    second commit

---
+++ views/nav-bar.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/styles/nav-bar.css">
+</head>
+
+<body>
+
+    <img src="./assets/img/logo200x50.png" alt="La Fritanguita Logo" class="logo">
+
+    <nav>
+        <ul class="nav__links">
+            <li><a href="#">INICIO</a></li>
+            <li><a href="#">CLIENTES</a></li>
+            <li><a href="#">EMPLEADOS</a></li>
+            <li><a href="#">PRODUCTOS</a></li>
+            <li><a href="#">CERRAR SESIÓN</a></li>
+            <li><a href="#">IR A LAFRITANGUITA.COM</a></li>
+        </ul>
+    </nav>
+
+    <a href="#" class="cta"><button>IR A LA FRITANGUITA.COM</button></a>
+
+</body>
+
+</html>
---
+++ views/template.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home - La Fritanguita Business Management System</title>
+</head>
+
+<body>
+
+    <header>
+
+        <!-- BARRA DE NAVEGACIÓN - NAVIGATION BAR -->
+        <?php
+
+        require_once("nav-bar.php");
+
+        ?>
+
+        <!-- TÍTULO DE LA PÁGINA -->
+        <h1>Welcome to the Business Management System</h1>
+
+        <!-- LOGO -->
+
+    </header>
+
+    <main>
+
+        <!-- CONTENIDO PRINCIPAL DEL PROYECTO-->
+
+    </main>
+
+    <aside>
+
+        <!-- ASIDE O BARRA LATERAL DE LA PÁGINA -->
+
+    </aside>
+
+    <footer>
+
+        <!-- PIÉ DE LA PÁGINA/PROYECTO -->
+
+    </footer>
+
+</body>
+
+</html>
